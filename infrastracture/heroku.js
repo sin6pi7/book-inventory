581dfba4b4770fb96c9738bb12a76de522402873
jacek-zyla <jacek.zyla@schibsted.pl>
2015-12-22 11:34:05 +0100

    Smart npm infra task.

--- infrastracture/heroku.js
+++ infrastracture/heroku.js
@@ -1,6 +1,7 @@
 const heroin = require('heroin-js');
+const path = require('path');
+
 const configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+const env = process.env.NODE_ENV || 'production';
 
-configurator(require('./test'));
-configurator(require('./prod'));
-
+configurator(require(path.join('.', env)));
