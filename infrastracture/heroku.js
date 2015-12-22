399a7ae5f4aadd2a9a019394e6a857117d951908
jacek-zyla <jacek.zyla@schibsted.pl>
2015-12-22 11:40:42 +0100

    Require infra config file properly.

--- infrastracture/heroku.js
+++ infrastracture/heroku.js
@@ -1,7 +1,6 @@
 const heroin = require('heroin-js');
-const path = require('path');
 
 const configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 const env = process.env.NODE_ENV || 'production';
 
-configurator(require(path.join('.', env)));
+configurator(require('./' + env));
